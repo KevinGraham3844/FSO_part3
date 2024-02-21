e8875c3d8293a1c46ac7d6208a29cffc48d4318e
Kevgraham384 <kev.graham384@gmail.com>
2024-02-21 09:39:56 -0800

    uibuild

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ let persons = [
     }
 ]
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+
 
 morgan.token('body', function getBody (req) {
     return JSON.stringify(req.body)
