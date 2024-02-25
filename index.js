ab684f2f93512d69e82ca7b01647410a59e60495
Kevgraham384 <kev.graham384@gmail.com>
2024-02-25 11:36:40 -0800

    uibuild

--- index.js
+++ index.js
@@ -53,54 +53,95 @@ app.get('/api/people', (request, response) => {
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    response.send(`Phonebook has info for ${persons.length} people
+    Person.find({})
+        .then(people => {
+            response.send(`Phonebook has info for ${people.length} people
                 <br/><br/>${date}`)
+        })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/people/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/people/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+          .then(result => {
+            response.status(204).end()
+          })
+          .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/people', (request, response) => {
     const body = request.body
-    const newId = Math.floor(Math.random() * 100000)
     
     
-    if (!body.name) {
+    if (body.name === undefined) {
         return response.status(400).json({
             error: 'name missing'
         })
-    } else if (!body.number) {
+    } else if (body.number === undefined) {
         return response.status(400).json({
             error: 'number missing'
         })
-    } else if (persons.filter(person => person.name === body.name).length !== 0) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
     }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+    
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/people/:id', (request, response, next) => {
+    const body = request.body
+
     const person = {
-        id: newId,
         name: body.name,
         number: body.number
     }
- 
-    persons = persons.concat(person)
-    response.json(person)
-   
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    console.log('this function was accessed when non existing route was requested')
+    response.status(404).send({ error: 'uknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.satus(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json( { error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
